650f2a28b3f0f230635effca90287f37c6160c98
celena <33563260+apomyxis@users.noreply.github.com>
2019-01-06 18:12:05 -0800

    lol maybe should test code before pushing it

--- calculateMetrics.py
+++ calculateMetrics.py
@@ -6,19 +6,20 @@ import textstat
 
 def main():
     parser = argparse.ArgumentParser(description="calculate some metrics for college cs department webpages")
-    parser.add_arugment("path", help="a directory containing text files of website contents")
+    parser.add_argument("path", help="a directory containing text files of website contents")
     parser.add_argument("outfile", help="a path to which to write a csv file containing calculated metrics")
     args = parser.parse_args()
 
     files = os.listdir(args.path)
 
-    for filename in files:
-        metrics = [f]
-        with open(path + filename) as f:
-            text = f.read()
-            metrics += textstat.flesch_kincaid_grade(text) # basic word/sentence-length based readability metric
-        writer = csv.writer(args.outfile)
-        writer.writerow(metrics)
+    with open(args.outfile, 'w', newline='') as outf:
+        for filename in files:
+            metrics = [filename]
+            with open(args.path + filename, 'r') as f:
+                text = f.read()
+                metrics.append(textstat.text_standard(text, float_output=True))
+            writer = csv.writer(outf)
+            writer.writerow(metrics)
     
 if __name__=='__main__':
     main()
