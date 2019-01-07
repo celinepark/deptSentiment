38253d65c4e9f593c5ae8b252cecc5bd9629ace6
Celine Park <cpark@g.hmc.edu>
2019-01-06 18:55:42 -0800

    punctuation stuff

--- calculateMetrics.py
+++ calculateMetrics.py
@@ -5,6 +5,30 @@ import string
 import textstat
 from textblob import TextBlob
 
+def punctuationMetric(sentenceList):
+    """ Takes in a TextBlob sentence list, returns the ratio of exclamation points to
+        total end punctuation
+    """
+    punctCount = 0
+    exclamationCount = 0
+    punctList = ['.', '!', '?']
+    ratio = 0
+
+    for sentence in sentenceList:
+        # grab last character (should be punctuation)
+        punct = sentence[-1]
+        if punct in punctList:
+            punctCount += 1
+            if punct == '!':
+                exclamationCount += 1
+    
+    if punctCount != 0:
+        ratio = exclamationCount/punctCount
+    
+    return ratio
+        
+
+
 def main():
     parser = argparse.ArgumentParser(description="calculate some metrics for college cs department webpages")
     parser.add_argument("path", help="a directory containing text files of website contents")
